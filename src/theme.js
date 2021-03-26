479d34e4579afaa2c177f2dd776bb61acc0fc0bb
Francisco Araujo <fjaraujo.dev@gmail.com>
2021-03-25 21:36:54 -0600

    Archivo de tema

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0277bd",
+    },
+    secondary: {
+      main: "#ef5350",
+    },
+  },
+});
+export default theme;
